bc421ed6aa22c7e026cf6e884b5c9bf0022a058a
zok2 <zok2@zok2deMac-mini.lan>
2024-06-29 01:01:37 +0800

    Currency struct update

--- models/funding/funding_models.go
+++ models/funding/funding_models.go
@@ -4,15 +4,30 @@ import "github.com/zok2/go-okx-v5"
 
 type (
 	Currency struct {
-		Ccy         string `json:"ccy"`
-		Name        string `json:"name"`
-		Chain       string `json:"chain"`
-		MinWd       string `json:"minWd"`
-		MinFee      string `json:"minFee"`
-		MaxFee      string `json:"maxFee"`
-		CanDep      bool   `json:"canDep"`
-		CanWd       bool   `json:"canWd"`
-		CanInternal bool   `json:"canInternal"`
+		Ccy                  string `json:"ccy"`                  // 币种名称，如 BTC
+		Name                 string `json:"name"`                 // 币种名称，不显示则无对应名称
+		LogoLink             string `json:"logoLink"`             // 币种Logo链接
+		Chain                string `json:"chain"`                // 币种链信息，如 USDT-ERC20，USDT-TRC20
+		CanDep               bool   `json:"canDep"`               // 当前是否可充值
+		CanWd                bool   `json:"canWd"`                // 当前是否可提币
+		CanInternal          bool   `json:"canInternal"`          // 当前是否可内部转账
+		MinDep               string `json:"minDep"`               // 币种单笔最小充值量
+		MinWd                string `json:"minWd"`                // 币种单笔最小链上提币量
+		MaxWd                string `json:"maxWd"`                // 币种单笔最大链上提币量
+		WdTickSz             string `json:"wdTickSz"`             // 提币精度,表示小数点后的位数
+		WdQuota              string `json:"wdQuota"`              // 过去24小时内提币额度，单位为USD
+		UsedWdQuota          string `json:"usedWdQuota"`          // 过去24小时内已用提币额度，单位为USD
+		MinFee               string `json:"minFee"`               // 普通地址最小提币手续费数量
+		MaxFee               string `json:"maxFee"`               // 普通地址最大提币手续费数量
+		MinFeeForCtAddr      string `json:"minFeeForCtAddr"`      // 合约地址最小提币手续费数量
+		MaxFeeForCtAddr      string `json:"maxFeeForCtAddr"`      // 合约地址最大提币手续费数量
+		MainNet              bool   `json:"mainNet"`              // 当前链是否为主链
+		NeedTag              bool   `json:"needTag"`              // 当前链是否需要标签（tag/memo）信息
+		MinDepArrivalConfirm string `json:"minDepArrivalConfirm"` // 充值到账最小网络确认数
+		MinWdUnlockConfirm   string `json:"minWdUnlockConfirm"`   // 提现解锁最小网络确认数
+		DepQuotaFixed        string `json:"depQuotaFixed"`        // 充币固定限额，单位为USD
+		UsedDepQuotaFixed    string `json:"usedDepQuotaFixed"`    // 已用充币固定额度，单位为USD
+		DepQuoteDailyLayer2  string `json:"depQuoteDailyLayer2"`  // Layer2网络每日充值上限
 	}
 	Balance struct {
 		Ccy       string `json:"ccy"`
